73255114656d2f378c9ead574ee7d57d60e49b0f
Insight-lab <insight-lab@Mac-mini-de-Insight-lab.local>
2021-05-19 00:29:47 -0300

    desafio 1: add, marker and remove completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,27 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    if (newTaskTitle === '') return;
+    setTasks( prevState => [
+      ...prevState, 
+      {  id: new Date().getTime(), 
+        title: newTaskTitle, 
+        done: false }
+    ]);
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const tasksUpdated = tasks.map(task => task.id === id ? 
+      { ...task, done: !task.done } : { ...task });
+    setTasks(tasksUpdated);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks(filteredTasks);
   }
 
   return (
